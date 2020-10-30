5a02da4ba2ad5d102ec0eab5ba9072d603bbfe8b
Jose Garcia <jgarcia@hypernovalabs.com>
2020-10-30 10:53:19 -0500

    Cambios finales

---
+++ oAuthTestProject/Controllers/OAuthController.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.Graph;
+using oAuthTestProject.Providers;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace oAuthTestProject.Controllers
+{
+    public class OAuthController : ApiController
+    {
+        [Route("PruebaOAuth")]
+        [HttpPost]
+        public async Task<HttpResponseMessage> GraphTestAsync()
+        {
+            MicrosoftGraphProvider _microsoftGraphProvider = new MicrosoftGraphProvider();
+            OAuthViewModel objresult = new OAuthViewModel();
+            string emailOrObjectId = ConfigurationManager.AppSettings["EmailOrObjectId"];
+            string fileNameOrObjectId = ConfigurationManager.AppSettings["FileNameOrObjectId"];
+
+            //conseguir usuario
+            var ResponseData = await _microsoftGraphProvider.GetIdByEmail(emailOrObjectId);
+
+            if (ResponseData.Status.Code != 200)
+            {
+                objresult.OnboardingOAuthData = new { ServiceResponse = false };
+                objresult.HttpResponse = new { Code = ResponseData.Status.Code, Message = "Error usuario: " + ResponseData.Status.Message };
+                return Request.CreateResponse(HttpStatusCode.OK, objresult);
+            }
+
+            //descargar archivo .docx
+            var ResponseDataFile = await _microsoftGraphProvider.GetFileStreamByUserId(ResponseData, emailOrObjectId, fileNameOrObjectId);
+
+            if (ResponseDataFile.Status.Code != 200)
+            {
+                objresult.OnboardingOAuthData = new { ServiceResponse = false };
+                objresult.HttpResponse = new { Code = ResponseDataFile.Status.Code, Message = "Error descarga: " + ResponseDataFile.Status.Message };
+                return Request.CreateResponse(HttpStatusCode.OK, objresult);
+            }
+
+            //Subir archivo .docx
+            var ResponseDataUpload = await _microsoftGraphProvider.UploadFileOneDrive(ResponseDataFile, emailOrObjectId, fileNameOrObjectId);
+
+            if (ResponseDataUpload.Status.Code != 200)
+            {
+                objresult.OnboardingOAuthData = new { ServiceResponse = false };
+                objresult.HttpResponse = new { Code = ResponseDataFile.Status.Code, Message = "Error subida: " + ResponseDataFile.Status.Message };
+                return Request.CreateResponse(HttpStatusCode.OK, objresult);
+            }
+
+            //Descargar como .pdf
+            var PdfResponseFile = await _microsoftGraphProvider.ConvertFileToPdf(ResponseDataUpload, emailOrObjectId, fileNameOrObjectId);
+
+            if (PdfResponseFile.Status.Code != 200)
+            {
+                objresult.OnboardingOAuthData = new { ServiceResponse = false };
+                objresult.HttpResponse = new { Code = ResponseDataUpload.Status.Code, Message = "Error descarga pdf: " + ResponseDataUpload.Status.Message };
+                return Request.CreateResponse(HttpStatusCode.OK, objresult);
+            }
+
+            objresult.OnboardingOAuthData = new { ServiceResponse = true };
+            objresult.HttpResponse = new { Code = ResponseData.Status.Code, Message = "Ok" };
+            return Request.CreateResponse(HttpStatusCode.OK, objresult);
+        }
+
+        public class OAuthViewModel
+        {
+            public Object HttpResponse { get; set; }
+            public object OnboardingOAuthData { get; set; }
+        }
+    }
+}
+
---
+++ oAuthTestProject/Providers/MicrosoftGraphProvider.cs
@@ -0,0 +1,180 @@
+﻿using Microsoft.Graph;
+using Microsoft.Graph.Auth;
+using Microsoft.Identity.Client;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace oAuthTestProject.Providers
+{
+    public interface IGraphProvider
+    {
+        Task<OAuthUserModel> GetIdByEmail(string email);
+        Task<OAuthUserModel> GetFileStreamByUserId(OAuthUserModel oAuth, string EmailOrObjectId, string FileNameOrObjectId);
+        Task<OAuthUserModel> UploadFileOneDrive(OAuthUserModel oAuth, string emailOrObjectId, string fileNameOrObjectId);
+        Task<OAuthUserModel> ConvertFileToPdf(OAuthUserModel oAuth, string emailOrObjectId, string fileNameOrObjectId);
+    }
+    public class MicrosoftGraphProvider : IGraphProvider
+    {
+        private string _tenantId { get; set; }
+        private string _clientId { get; set; }
+        private string _clientSecret { get; set; }
+
+        public MicrosoftGraphProvider()
+        {
+            _tenantId = ConfigurationManager.AppSettings["TenantId"];
+            _clientId = ConfigurationManager.AppSettings["ClientId"];
+            _clientSecret = ConfigurationManager.AppSettings["ClientSecret"];
+        }
+
+        public async Task<OAuthUserModel> GetIdByEmail(string EmailOrObjectId)
+        {
+            OAuthUserModel model = new OAuthUserModel();
+            try
+            {
+                var confidentialClient = ConfidentialClientApplicationBuilder
+                .Create(_clientId)
+                .WithTenantId(_tenantId)
+                .WithClientSecret(_clientSecret)
+                .Build();
+                ClientCredentialProvider authProvider = new ClientCredentialProvider(confidentialClient);
+                GraphServiceClient graphClient = new GraphServiceClient(authProvider);
+                var user = await graphClient.Users[EmailOrObjectId].Request().GetAsync();
+
+                model.User = user;
+                model.AuthProvider = authProvider;
+                model.GraphService = graphClient;
+                model.Status.Code = 200;
+                model.Status.Message = "OK";
+
+                return model;
+            }
+            catch (Exception e)
+            {
+                model.Status.Code = 406;
+                model.Status.Message = e.Message;
+                return model;
+            }
+        }
+
+        public async Task<OAuthUserModel> GetFileStreamByUserId(OAuthUserModel oAuth, string emailOrObjectId, string fileNameOrObjectId)
+        {
+            try
+            {
+                Stream templateFileStream = await oAuth.GraphService.Users[emailOrObjectId].Drive.Items[fileNameOrObjectId].Content.Request().GetAsync();
+
+                oAuth.TemplateStream = templateFileStream;
+                oAuth.Status.Code = 200;
+                oAuth.Status.Message = "OK";
+
+                return oAuth;
+            }
+            catch (ServiceException e)
+            {
+                oAuth.Status.Code = 406;
+                oAuth.Status.Message = e.Message;
+                return oAuth;
+            }
+        }
+
+        public async Task<OAuthUserModel> UploadFileOneDrive(OAuthUserModel oAuth, string emailOrObjectId, string fileNameOrObjectId)
+        {
+            try
+            {
+                string _newFileName = $"Prueba Onboarding-{Guid.NewGuid()}.docx";
+
+                // where you want to save the file, with name
+                var item = $"/Contratos/" + _newFileName;
+
+                var uploadSession = await GetUploadSession(oAuth, item, emailOrObjectId);
+
+                var maxChunkSize = 320 * 1024; // 320 KB - Change this to your chunk size. 5MB is the default.
+                var provider = new ChunkedUploadProvider(uploadSession, oAuth.GraphService, oAuth.TemplateStream, maxChunkSize);
+
+                // Setup the chunk request necessities
+                var chunkRequests = provider.GetUploadChunkRequests();
+                var readBuffer = new byte[maxChunkSize];
+                var trackedExceptions = new List<Exception>();
+                DriveItem itemResult = null;
+
+                //upload the chunks
+                foreach (var request in chunkRequests)
+                {
+                    // Do your updates here: update progress bar, etc.
+                    // ...
+                    // Send chunk request
+                    var result = await provider.GetChunkRequestResponseAsync(request, readBuffer, trackedExceptions);
+
+                    if (result.UploadSucceeded)
+                    {
+                        itemResult = result.ItemResponse;
+                    }
+                }
+
+                oAuth.Status.Code = 200;
+                oAuth.Status.Message = "OK";
+
+                return oAuth;
+            }
+            catch (Exception e)
+            {
+                oAuth.Status.Code = 406;
+                oAuth.Status.Message = e.Message;
+                return oAuth;
+            }
+        }
+        public async Task<OAuthUserModel> ConvertFileToPdf(OAuthUserModel oAuth, string emailOrObjectId, string fileNameOrObjectId)
+        {
+            try
+            {
+                var queryOptions = new List<QueryOption>()
+                {
+                    new QueryOption("format", "pdf")
+                };
+
+                var stream = await oAuth.GraphService.Users[emailOrObjectId].Drive.Items[fileNameOrObjectId].Content.Request(queryOptions).GetAsync();
+
+                oAuth.PdfStream = stream;
+                oAuth.Status.Code = 200;
+                oAuth.Status.Message = "Ok";
+
+                return oAuth;
+            }
+            catch (Exception e)
+            {
+                oAuth.Status.Code = 406;
+                oAuth.Status.Message = e.Message;
+                return oAuth;
+            }
+        }
+        public async Task<UploadSession> GetUploadSession(OAuthUserModel oAuth, string item, string user)
+        {
+            return await oAuth.GraphService.Users[user].Drive.Root.ItemWithPath(item).CreateUploadSession().Request().PostAsync();
+        }
+    }
+
+    public class OAuthUserModel
+    {
+        public User User { get; set; }
+        public ClientCredentialProvider AuthProvider { get; set; }
+        public GraphServiceClient GraphService { get; set; }
+        public Stream TemplateStream { get; set; }
+        public Stream PdfStream { get; set; }
+        public OAuthStatusModel Status { get; set; }
+        public OAuthUserModel()
+        {
+            User = new User();
+            GraphService = new GraphServiceClient(AuthProvider);
+            Status = new OAuthStatusModel();
+        }
+    }
+    public class OAuthStatusModel
+    {
+        public int Code { get; set; }
+        public string Message { get; set; }
+    }
+}
